c63aa4803477af633511ac2445e219eef3bd1e6d
Joshua Bohn <500200388@fvtc.edu>
2024-04-13 22:42:43 -0500

    Completed Assignment 7

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const todos = [
 	{ id: 3, item: 'Build a To Do App', complete: false }
 ]
 
-app.get('/', (_, response) => {
+app.get('/', (request, response) => {
 	response.sendFile('index.html', { root })
 })
 
@@ -21,10 +21,28 @@ app.get('/', (_, response) => {
 
 // GET /api/todos
 
+app.get('/api/todos', (request, response) => {
+	response.json(todos);
+})
+
 // POST /api/todos
+
+app.post('/api/todos', (request, response) => {
+	const { item } = request.body
+	const id = todos.length + 1
+	const complete = false
+	todos.push({ id, item, complete })
+	response.json({ id });
+})
 
 // PUT /api/todos/:id
 
+app.put('/api/todos/:id', (request, response) => {
+	const { id } = request.params
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete // toggle the complete property
+	response.json({ id: task.id, complete: task.complete });
+})
 
 
 const message = `Server running: http://localhost:${port}`
