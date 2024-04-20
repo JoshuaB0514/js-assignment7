338e29f5bb6eac3c0c0fcc76a3ad5a98a5a99f7d
Joshua Bohn <500200388@fvtc.edu>
2024-04-20 08:39:57 -0500

    Updated app.js, added api-endpoints.js, and added secrets folder

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use('/api/todos', require('./api-routes'))
 app.use(express.static('public'))
 
 const todos = [
@@ -16,34 +17,6 @@ const todos = [
 app.get('/', (request, response) => {
 	response.sendFile('index.html', { root })
 })
-
-
-
-// GET /api/todos
-
-app.get('/api/todos', (request, response) => {
-	response.json(todos);
-})
-
-// POST /api/todos
-
-app.post('/api/todos', (request, response) => {
-	const { item } = request.body
-	const id = todos.length + 1
-	const complete = false
-	todos.push({ id, item, complete })
-	response.json({ id });
-})
-
-// PUT /api/todos/:id
-
-app.put('/api/todos/:id', (request, response) => {
-	const { id } = request.params
-	const task = todos.find(todo => todo.id.toString() === id)
-	task.complete = !task.complete // toggle the complete property
-	response.json({ id: task.id, complete: task.complete });
-})
-
 
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
